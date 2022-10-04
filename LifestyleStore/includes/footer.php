912f38359a874d1d547bcb5d647f11de32aef7a6
Prakhar Srivastava <64420033+praks3@users.noreply.github.com>
2022-10-04 23:49:11 +0530

    Add files via upload

---
+++ LifestyleStore/includes/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div class="container">
+        <p style="font-size:12px;" align="center">Copyright © Lifestyle Store. All Rights Reserved | Contact Us: +91 90000 00000</p>
+        <p style="font-size:12px;" align="center">This site is developed by <a href="https://www.linkedin.com/in/divyanshi-srivastava71">Divyanshi Srivastava</a> & <a href="https://www.linkedin.com/in/praks03">Prakhar Srivastava</a> </p>
+    </div>
+</footer>
